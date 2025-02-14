52d8d38b46d352c25830692e9ef0e0286bd7f090
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-14 17:36:43 -0300

    Add area types

--- src/constants/areas/index.d.ts
+++ src/constants/areas/index.d.ts
@@ -4,4 +4,15 @@ export interface IArea {
     title: string;
     goals: number;
     icon: Icons;
+    route: `/${Types}`;
 }
+
+export type Types = (
+    'pessoais' |
+    'familiares' |
+    'sociais' |
+    'intelectuais' |
+    'espirituais' |
+    'profissionais' |
+    'financeiras'
+);
--- src/constants/areas/index.ts
+++ src/constants/areas/index.ts
@@ -1,43 +1,61 @@
-import {IArea} from '@/constants/areas/index.d';
+import {IArea, Types} from '@/constants/areas/index.d';
 
 const AREAS: IArea[] = [
     {
         goals: 0,
         icon: 'user',
         title: 'Pessoal',
+        route: '/pessoais',
     },
     {
         goals: 0,
         icon: 'heart',
         title: 'Familiar',
+        route: '/familiares',
     },
     {
         goals: 0,
         icon: 'users',
         title: 'Social',
+        route: '/sociais',
     },
     {
         goals: 0,
         icon: 'book',
         title: 'Intelectual',
+        route: '/intelectuais',
     },
     {
         goals: 0,
         icon: 'sun',
         title: 'Espiritual',
+        route: '/espirituais',
     },
     {
         goals: 0,
         icon: 'briefcase',
         title: 'Profissional',
+        route: '/profissionais',
     },
     {
         goals: 0,
         icon: 'dollar-sign',
         title: 'Financeiro',
+        route: '/financeiras',
     },
+];
+
+const TYPES: Types[] = [
+    'pessoais',
+    'familiares',
+    'sociais',
+    'intelectuais',
+    'espirituais',
+    'profissionais',
+    'financeiras'
 ];
 
 export {
     AREAS,
+    TYPES,
 };
