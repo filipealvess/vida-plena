4bcccd5cfb29d96c15c728a45be35af8b332d742
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-14 11:41:37 -0300

    Add index page

---
+++ src/core/pages/Index/index.tsx
@@ -0,0 +1,38 @@
+import AreaCard from "@/components/Cards/Area";
+import Logo from "@/components/Logo";
+import styles from '@/core/pages/Index/styles.module.css';
+import { AREAS } from "@/constants/areas";
+import { useEffect, useState } from "react";
+
+import {IArea} from '@/constants/areas/index.d';
+
+function IndexPage() {
+    const [areas, setAreas] = useState<IArea[]>([]);
+
+    useEffect(() => {
+        setAreas(AREAS);
+    }, []);
+
+    return (
+        <main className={styles.container}>
+            <Logo />
+
+            <div className={styles.content}>
+                <h2>Organize sua vida por etapas</h2>
+
+                <section>
+                    {areas.map(area => (
+                        <AreaCard
+                            key={area.title}
+                            goals={area.goals}
+                            title={area.title}
+                            icon={area.icon}
+                        />
+                    ))}
+                </section>
+            </div>
+        </main>
+    );
+}
+
+export default IndexPage;
