aa5b3cfcc7a10f919c9e62e0a5699cc2ae7da7fb
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-14 11:40:34 -0300

    Add areas constant

---
+++ src/constants/areas/index.d.ts
@@ -0,0 +1,7 @@
+import {Icons} from '@/components/Icon/index.d';
+
+export interface IArea {
+    title: string;
+    goals: number;
+    icon: Icons;
+}
---
+++ src/constants/areas/index.ts
@@ -0,0 +1,43 @@
+import {IArea} from '@/constants/areas/index.d';
+
+const AREAS: IArea[] = [
+    {
+        goals: 0,
+        icon: 'user',
+        title: 'Pessoal',
+    },
+    {
+        goals: 0,
+        icon: 'heart',
+        title: 'Familiar',
+    },
+    {
+        goals: 0,
+        icon: 'users',
+        title: 'Social',
+    },
+    {
+        goals: 0,
+        icon: 'book',
+        title: 'Intelectual',
+    },
+    {
+        goals: 0,
+        icon: 'sun',
+        title: 'Espiritual',
+    },
+    {
+        goals: 0,
+        icon: 'briefcase',
+        title: 'Profissional',
+    },
+    {
+        goals: 0,
+        icon: 'dollar-sign',
+        title: 'Financeiro',
+    },
+];
+
+export {
+    AREAS,
+};
