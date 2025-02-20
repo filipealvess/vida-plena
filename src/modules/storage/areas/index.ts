0e901a229fa5124225602c87d96acf156e6ff68b
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-20 12:48:28 -0300

    Add storage util to remove goal id from an area

--- src/modules/storage/areas/index.ts
+++ src/modules/storage/areas/index.ts
@@ -5,7 +5,7 @@ import { Types } from "@/constants/areas/index.d";
 import { IStoredAreasById } from "@/modules/storage/areas/index.d";
 
 function addGoal(area: Types, id: string) {
-    const goals = storage.get<IStoredAreasById>(STORAGE.AREAS) ?? {};
+    const goals = list();
 
     goals[area] = {
         ...goals[area],
@@ -15,11 +15,29 @@ function addGoal(area: Types, id: string) {
         ],
     };
 
-    storage.set(STORAGE.AREAS, goals);
+    storage.set<IStoredAreasById>(STORAGE.AREAS, goals);
+}
+
+function removeGoal(area: Types, id: string) {
+    const areas = list();
+    const found = areas[area];
+
+    if (found === undefined) {
+        return;
+    }
+
+    const updated = found.goals.filter(goal => goal !== id);
+
+    storage.set<IStoredAreasById>(STORAGE.AREAS, {
+        ...areas,
+        [area]: {
+            goals: updated,
+        },
+    });
 }
 
 function get(area: string) {
-    const goals = storage.get<IStoredAreasById>(STORAGE.AREAS) ?? {};
+    const goals = list();
 
     return goals[area] ?? null;
 }
@@ -32,6 +50,7 @@ function list() {
 
 const areas = {
     addGoal,
+    removeGoal,
     get,
     list,
 };
