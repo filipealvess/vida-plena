837bba252780b39dd6cf68422ea7cdbe3046b1c0
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-20 12:54:06 -0300

    Add goal page

--- src/core/Routes.tsx
+++ src/core/Routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "@/core/pages/Index";
 import AreaPage from "@/core/pages/Area";
 import NewGoalPage from "@/core/pages/NewGoal";
+import GoalPage from "@/core/pages/Goal";
 
 function Router() {
     return (
@@ -9,6 +10,7 @@ function Router() {
             <Route path="/" Component={IndexPage} />
             <Route path="/metas/:area" Component={AreaPage} />
             <Route path="/metas/:area/cadastro" Component={NewGoalPage} />
+            <Route path="/metas/:area/:goal" Component={GoalPage} />
         </Routes>
     );
 }
---
+++ src/core/pages/Goal/index.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "@/components/Header";
+import styles from '@/core/pages/Goal/styles.module.css';
+import Dropdown from '@/core/pages/Goal/components/Dropdown';
+import storage from "@/modules/storage";
+import InlineCard from "@/components/Cards/Inline";
+import { getProgress } from "@/utils/goals";
+import { enToPtBr } from "@/utils/dates";
+import { checkArea } from "@/utils/areas";
+import Divider from "@/components/Divider";
+import Checkbox from "@/components/Form/Checkbox";
+
+import { IStoredChecklist, IStoredChecklistById, IStoredGoal } from "@/modules/storage/goals/index.d";
+
+function GoalPage() {
+    const [data, setData] = useState<IStoredGoal | null>(null);
+    const [checklist, setChecklist] = useState({} as IStoredChecklistById);
+
+    const params = useParams();
+    const navigate = useNavigate();
+
+    function handleCheck(id: number) {
+        const item = checklist[id];
+
+        if (item === undefined) {
+            return;
+        }
+
+        setChecklist(prev => {
+            const updated = {
+                ...prev,
+                [id]: {
+                    ...prev[id],
+                    completed: !prev[id].completed,
+                },
+            };
+
+            storage.goals.updateChecklist(params.goal!, updated);
+
+            return updated;
+        });
+    }
+
+    useEffect(() => {
+        if (checkArea(params.area) === false) {
+            navigate("/", { replace: true });
+            return;
+        }
+
+        if (params.goal === undefined) {
+            navigate(`/metas/${params.area}`, { replace: true });
+            return;
+        }
+
+        const goal = storage.goals.get(params.goal);
+
+        if (goal === null) {
+            navigate(`/metas/${params.area}`, { replace: true });
+            return;
+        }
+
+        setData(goal);
+        setChecklist(goal.checklist);
+
+        document.title = `${goal.name} - Vida Plena`;
+    }, [params.area]);
+
+    if (params.goal === undefined || data === null) {
+        return (
+            <p>Carregando...</p>
+        );
+    }
+
+    return (
+        <main className={styles.container}>
+            <Header
+                title='Detalhes da meta'
+                backRoute={`/metas/${params.area}`}
+                suffix={<Dropdown goal={data} />}
+            />
+
+            <section className={styles.data}>
+                <h2>{data.name}</h2>
+
+                <div className={styles.cards}>
+                    <InlineCard icon="check-circle">
+                        <strong>{getProgress(Object.values(checklist))} </strong>
+                        concluído
+                    </InlineCard>
+
+                    <InlineCard icon="calendar">
+                        até
+                        <strong> {enToPtBr(data.endDate)}</strong>
+                    </InlineCard>
+                </div>
+
+                <Divider title="Checklist" className={styles.divider} />
+
+                {Object.keys(checklist).length === 0 && (
+                    <p className={styles.feedback}>
+                        Nenhum item cadastrado
+                    </p>
+                )}
+
+                {Object.values(checklist).map((item: IStoredChecklist) => (
+                    <Checkbox
+                        key={item.id}
+                        checked={item.completed}
+                        label={item.text}
+                        onChange={() => handleCheck(item.id)}
+                    />
+                ))}
+            </section>
+        </main>
+    );
+}
+
+export default GoalPage;
