23bccf2a277488add8e31ea3fbdbda60570cfe27
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-20 12:46:49 -0300

    Fix util that create goal id

--- src/modules/storage/goals/utils.ts
+++ src/modules/storage/goals/utils.ts
@@ -1,6 +1,6 @@
 function getId(timestamp: string, name: string) {
     const cleanTimestamp = timestamp.replace(/\D/g, '')
-    const cleanName = name.normalize('NFD').replace(/[^a-zA-Z0-9]/g, '');
+    const cleanName = name.normalize('NFD').replace(/[^a-zA-Z0-9\s]/g, '');
     const separatedName = cleanName.replace(/\s/g, '-');
 
     return `${cleanTimestamp}_${separatedName}`;
