49c23accb79dc858a049cace3eebade564a8f855
Filipe Alves <filipe.alvesdsouza@gmail.com>
2025-02-26 16:46:19 -0300

    Display UI based on selected theme

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,7 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "@/core/Routes";
+import { useEffect } from "react";
+import { updateUI } from "@/utils/theme";
 
 function App() {
+    useEffect(() => {
+        updateUI();
+    }, []);
+
     return (
         <BrowserRouter>
             <Router />
--- src/core/pages/Index/index.tsx
+++ src/core/pages/Index/index.tsx
@@ -4,11 +4,20 @@ import styles from '@/core/pages/Index/styles.module.css';
 import { AREAS } from "@/constants/areas";
 import { useEffect, useState } from "react";
 import storage from "@/modules/storage";
+import { updateUI } from "@/utils/theme";
 
 import {IArea} from '@/constants/areas/index.d';
 
 function IndexPage() {
     const [areas, setAreas] = useState<IArea[]>([]);
+
+    function handleChangeTheme() {
+        const current = storage.theme.get() ?? 'light';
+
+        storage.theme.set(current === 'light' ? 'dark' : 'light');
+
+        updateUI();
+    }
 
     useEffect(() => {
         const stored = storage.areas.list();
@@ -23,7 +32,13 @@ function IndexPage() {
 
     return (
         <main className={styles.container}>
-            <Logo />
+            <div className={styles.header}>
+                <Logo />
+
+                <button onClick={handleChangeTheme}>
+                    Mudar tema
+                </button>
+            </div>
 
             <div className={styles.content}>
                 <h2>Organize sua vida por etapas</h2>
